5f2fd76933822504e3d8d8f8cac6450aeaa9f45f
robbch <robertochello@gmail.com>
2019-11-06 12:18:19 +0100

    Commit post merge

---
+++ src/main/java/com/unicampania/xmltodb/config/aclass_preparedstatmentsetter/AcOverviewPreparedStatmentSetter.java
@@ -0,0 +1,28 @@
+package com.unicampania.xmltodb.config.aclass_preparedstatmentsetter;
+
+import com.unicampania.xmltodb.model.aclass.AClass;
+import org.springframework.batch.item.database.ItemPreparedStatementSetter;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AcOverviewPreparedStatmentSetter implements ItemPreparedStatementSetter<AClass> {
+
+
+    @Override
+    public void setValues(AClass item, PreparedStatement ps) throws SQLException {
+        for (int i = 0; i < item.getAcOverview().getP().size(); i++) {
+
+            ps.setString(1, item.getAcOverview().getP().get(i).getType());
+            ps.setString(2, item.getId().concat(Integer.toString(i)));
+            ps.setString(3, String.valueOf(item.getAcOverview()
+                    .getP().get(i)
+                    .getValue())
+                    .replace("[", "")
+                    .replace("]", ""));
+            ps.setString(4, item.getId());
+            ps.setString(5, item.getId().concat(Integer.toString(i)));
+           // ps.addBatch();
+        }
+    }
+}
